7ff495e7673d1781eae3b1ea5f18d040de367dcf
Ali Esat Güngör <78855338+aliesatgungor@users.noreply.github.com>
2022-11-26 18:30:16 +0300

    Add files via upload

--- MainWindow.py
+++ MainWindow.py
@@ -1,39 +1,44 @@
-import time
 import gi
+import time
 import datetime
-gi.require_version('Gtk', '3.0')
+import subprocess
 from gi.repository import Gtk, GLib, Gio
 
-import subprocess
+
+gi.require_version('Gtk', '3.0')
+
+
+
 
 class MainWindow:
+
     def __init__(self, app):
-        # Gtk Builder
+
         self.builder = Gtk.Builder()
 
-        # Import UI file:
+
         self.builder.add_from_file("MainWindow.glade")
         self.builder.connect_signals(self)
 
-        # Window
+
         self.window = self.builder.get_object("window")
         self.window.set_application(app)
         
-        # Glade'deki elemanlari kullanmak icin degiskene atayalim:
+
         self.defineComponents()
         
-        # Show window
+
         self.window.show_all()
     
     def defineComponents(self):
         self.output_label = self.builder.get_object("output_label")
-    
+
     def on_free_ram_clicked(self, btn):
         process = subprocess.run(["free","-h"], capture_output=True)
         output = process.stdout.decode("utf-8")
         
         self.output_label.set_label(output)
-    
+
     def on_clean_cache_clicked(self, btn):
         process = subprocess.run(["echo", "4"," | ","tee"," /proc/sys/vm/drop_caches"], capture_output=True)
         process = subprocess.run(["echo", "3"," | ","tee"," /proc/sys/vm/drop_caches"], capture_output=True)
@@ -42,9 +47,35 @@ class MainWindow:
         output = process.stdout.decode("utf-8")
         o = "Tamamlandı!"
         self.output_label.set_label(o)
+
     def on_kernel_version_clicked(self, btn):
         process = subprocess.run(["uname","-r"], capture_output=True)
         output = process.stdout.decode("utf-8")
         
         self.output_label.set_label(output)
-        
+    def on_sistemi_kapat_clicked(self,btn):
+        komut = "systemctl poweroff -i"
+        process = subprocess.run(komut.split(),capture_output=True)    
+        output = process.stdout.decode("utf-8")
+
+        self.output_label.set_label(output)
+    def on_reboot_clicked(self,btn):
+        komut = "systemctl reboot now"
+        process = subprocess.run(komut.split(),capture_output= True)
+        output = process.stdout.decode("utf-8") 
+        self.output_label.set_label(output)   
+    def on_logout_clicked(self,btn):
+        komut = "logout"
+        process = subprocess.run(komut.split(),capture_output=True)
+        output = process.stdout.decode("utf-8")   
+        self.output_label.set_label(output) 
+    def on_username_clicked(self,btn):
+        komut = "whoami"
+        process = subprocess.run(komut.split(),capture_output=True) 
+        output = process.stdout.decode("utf-8")  
+        self.output_label.set_label(output) 
+    def on_disc_usage_clicked(self,btn):
+        komut = "df -h"
+        process = subprocess.run(komut.split(),capture_output=True)    
+        output = process.stdout.decode("utf-8")
+        self.output_label.set_label(output)
